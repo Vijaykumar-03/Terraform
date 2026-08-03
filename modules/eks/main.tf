65d64aed8752122888558f90d4d49e4ee3fb8a9f
Vijaykumar-03 <paxsaat03@gmail.com>
2026-08-03 23:03:51 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,42 +1,30 @@
 module "eks" {
-
-  source = "terraform-aws-modules/eks/aws"
-
-  version = "~>21.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.24"
 
   name               = var.cluster_name
   kubernetes_version = "1.33"
 
+  vpc_id     = data.aws_vpc.default.id
   subnet_ids = data.aws_subnets.default.ids
 
-  vpc_id = data.aws_vpc.default.id
-
   eks_managed_node_groups = {
-
     worker = {
-
-      desired_size = 2
-
-      max_size = 3
-
-      min_size = 1
-
       instance_types = ["t3.medium"]
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 3
     }
   }
 }
 
 data "aws_vpc" "default" {
-
   default = true
 }
 
 data "aws_subnets" "default" {
-
   filter {
-
-    name="vpc-id"
-
-    values=[data.aws_vpc.default.id]
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
