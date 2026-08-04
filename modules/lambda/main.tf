36cf4575232beba2fbb4e0f0156a2dd9929b9bd6
Vijaykumar-03 <paxsaat03@gmail.com>
2026-08-04 20:00:16 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,23 +1,23 @@
 data "archive_file" "zip" {
-
-  type="zip"
-
-  source_dir="../lambda-code"
-
-  output_path="../lambda.zip"
+  type        = "zip"
+  source_dir  = "${path.root}/lambda-code"
+  output_path = "${path.root}/lambda.zip"
 }
 
 resource "aws_lambda_function" "lambda" {
-
   function_name = var.function_name
 
-  filename = data.archive_file.zip.output_path
+  filename         = data.archive_file.zip.output_path
+  source_code_hash = data.archive_file.zip.output_base64sha256
 
-  source_code_hash=data.archive_file.zip.output_base64sha256
+  role    = var.role_arn
+  handler = "lambda_function.lambda_handler"
+  runtime = "python3.12"
 
-  handler="lambda_function.lambda_handler"
+  timeout     = 30
+  memory_size = 128
+}
 
-  runtime="python3.12"
-
-  role=var.role_arn
+output "lambda_arn" {
+  value = aws_lambda_function.lambda.arn
 }
