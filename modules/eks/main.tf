4900aabb2f654caaa271c519a0b36c52b770ebed
Vijaykumar-03 <paxsaat03@gmail.com>
2026-08-05 16:23:33 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,3 +1,19 @@
+resource "aws_launch_template" "eks_nodes" {
+  name_prefix = "eks-worker-"
+
+  key_name = "terraform-key"
+
+  update_default_version = true
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "eks-worker"
+    }
+  }
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.24"
@@ -5,21 +21,29 @@ module "eks" {
   name               = var.cluster_name
   kubernetes_version = "1.33"
 
+  authentication_mode = "API_AND_CONFIG_MAP"
+
+  endpoint_public_access  = true
+  endpoint_private_access = false
+
+  enable_irsa = true
+
   vpc_id     = data.aws_vpc.default.id
   subnet_ids = data.aws_subnets.default.ids
-
-  endpoint_public_access = true
 
   eks_managed_node_groups = {
     worker = {
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["t3.micro"]
 
+      desired_size = 2
       min_size     = 1
       max_size     = 3
-      desired_size = 2
 
       capacity_type = "ON_DEMAND"
+
+      launch_template_id      = aws_launch_template.eks_nodes.id
+      launch_template_version = "$Latest"
 
       labels = {
         role = "worker"
